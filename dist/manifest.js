fdeff765bf8db36078fc9ee311191cdd1276b3bf
christopherbutlercole <christopherbutlercole@gmail.com>
2026-09-11 14:13:47 +0100

    Add files via upload

---
+++ dist/manifest.js
@@ -0,0 +1,39 @@
+window.IMAGE_MANIFEST = {
+  "violin-1": {
+    "1": [
+      "images/violin-1/1/demo-1.svg",
+      "images/violin-1/1/demo-2.svg",
+      "images/violin-1/1/demo-3.svg"
+    ],
+    "2": [
+      "images/violin-1/2/demo-1.svg",
+      "images/violin-1/2/demo-2.svg",
+      "images/violin-1/2/demo-3.svg"
+    ],
+    "3": [
+      "images/violin-1/3/demo-1.svg",
+      "images/violin-1/3/demo-2.svg",
+      "images/violin-1/3/demo-3.svg"
+    ],
+    "4": [
+      "images/violin-1/4/demo-1.svg",
+      "images/violin-1/4/demo-2.svg",
+      "images/violin-1/4/demo-3.svg"
+    ],
+    "5": [
+      "images/violin-1/5/demo-1.svg",
+      "images/violin-1/5/demo-2.svg",
+      "images/violin-1/5/demo-3.svg"
+    ],
+    "6": [
+      "images/violin-1/6/demo-1.svg",
+      "images/violin-1/6/demo-2.svg",
+      "images/violin-1/6/demo-3.svg"
+    ],
+    "7": [
+      "images/violin-1/7/demo-1.svg",
+      "images/violin-1/7/demo-2.svg",
+      "images/violin-1/7/demo-3.svg"
+    ]
+  }
+};
